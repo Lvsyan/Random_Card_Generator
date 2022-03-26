1648cdd0d436028b13a8a895dff0c09b01b55109
Lvsyan <Lvsyan96@gmail.com>
2022-03-26 12:42:23 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -3,18 +3,39 @@ import "@popperjs/core";
 import "bootstrap";
 import "./style.css";
 
+//Constantes o variables
 let mezcla = [];
 let position = 1;
 let suits = ["♦", "♥", "♠", "♣"];
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-for (let palos of cards) {
-  for (let numeros of suits) {
-    let num = [palos, numeros];
-    mezcla.push(num);
-    mezcla.sort(() => Math.random() - 0.5);
+//Funcion para recorrer cartas y palos al azar
+const generaCarta = () => {
+  for (let palos of cards) {
+    for (let numeros of suits) {
+      let num = [palos, numeros];
+      mezcla.push(num);
+      mezcla.sort(() => Math.random() - 0.5);
+    }
   }
-}
+  return mezcla;
+};
+generaCarta();
+
+const aleatorio = () => {
+  mezcla.sort(() => Math.random() - 0.5);
+  return mezcla;
+};
+
+const borrarCarta = () => {
+  mezcla = aleatorio(mezcla);
+  position = 0;
+  document.querySelector("#numero").innerHTML = "";
+  document.querySelector("#topSuit").innerHTML = "";
+  document.querySelector("#botSuit").innerHTML = "";
+  start.disabled = false;
+};
+
 const deckCards = () => {
   let distribuir = mezcla[position];
   document.querySelector("#numero").innerHTML = distribuir[0];
@@ -30,7 +51,9 @@ const deckCards = () => {
   }
 };
 
+//Window onload
 window.onload = function() {
   //write your code her
   start.addEventListener("click", deckCards);
+  reset.addEventListener("click", borrarCarta);
 };
